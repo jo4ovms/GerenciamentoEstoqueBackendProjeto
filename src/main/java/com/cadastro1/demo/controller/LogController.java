0ec8b9576c22624043f0f057160dfa192a480ec4
jo4ovms <joaovictormacields@gmail.com>
2024-06-06 16:13:20 -0300

    adicionado filtro de data no relatorio

--- src/main/java/com/cadastro1/demo/controller/LogController.java
+++ src/main/java/com/cadastro1/demo/controller/LogController.java
@@ -7,8 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -22,6 +25,14 @@ public class LogController {
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<List<Log>> listarLogs() {
         List<Log> logs = logService.listar();
+        return ResponseEntity.ok(logs);
+    }
+
+    @GetMapping("/por-data")
+    public ResponseEntity<List<Log>> listarLogsPorData(@RequestParam String startDate, @RequestParam String endDate) {
+        LocalDateTime start = LocalDate.parse(startDate).atStartOfDay();
+        LocalDateTime end = LocalDate.parse(endDate).atTime(23, 59, 59);
+        List<Log> logs = logService.listarLogsPorData(start, end);
         return ResponseEntity.ok(logs);
     }
 
